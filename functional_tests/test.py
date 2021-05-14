8f7e32d86eaccc8bfdebad03282d6db57669c653
Ryanuppp <835407339@qq.com>
2021-05-14 13:34:14 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -11,11 +11,16 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -33,33 +38,27 @@ class NewVisitorTest(unittest.TestCase):
         
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-
-        # inputBox.send_keys('Buy peacock feathers')
+        inputBox.send_keys('Buy peacock feathers')
+       
     
         # when she hits enter, the page updates, and now the page lists
         # "1： Buy peacock feathers" as an item in a to-do list
-
-        # inputBox.send_keys(Keys.ENTER)
-        # time.sleep(1)
-
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        inputBox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"(Edith is very methodical)
 
+        inputBox = self.browser.find_element_by_id('id_new_item')
         inputBox.send_keys('Use peacock feathers to make a fly')
         inputBox.send_keys(Keys.ENTER)
         time.sleep(1)
         
         # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2:Use peacock feathers to make a fly',
-                      [row.text for row in rows])
 
         # Edith wonder whether the site will remember her list. The she sees
         # that the site has generated a unique URL for her -- there is some
@@ -69,7 +68,6 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
         # Satisfied, she goes back to sleep
         
-if __name__ == '__main__':
-    unittest.main(warnings = 'ignore')
+
 
        
