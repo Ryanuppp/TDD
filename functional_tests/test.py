99e216c30e917f9a4f694d1193c028a48c58bcab
Ryanuppp <835407339@qq.com>
2021-06-09 14:46:36 +0800

    Hack FT runner to be able to test staging

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
+import os
 
 MAX_WAIT = 10
 
@@ -10,6 +11,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Chrome()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.refresh()
@@ -27,6 +31,51 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 if time.time() -start_time >MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+    
+    def test_can_start_a_list_for_one_user(self):
+
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        # She is invited to enter a to-do item straight away
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputBox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+        
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputBox.send_keys('Buy peacock feathers')
+       
+    
+        # when she hits enter, the page updates, and now the page lists
+        # "1： Buy peacock feathers" as an item in a to-do list
+        inputBox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly"(Edith is very methodical)
+        time.sleep(1)
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        inputBox.send_keys('Use peacock feathers to make a fly')
+        inputBox.send_keys(Keys.ENTER)
+        
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+        self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
+        
+        #satisfied, she goes back to sleep
+        
+    
+
 
     def test_multiple_users_can_start_lists_at_different_url(self):
         #Edith starts a new to-do list
@@ -70,49 +119,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('Buy peacock_feathers', page_text)
         self.assertIn('Buy milk', page_text)
         
-        
-        
-    def test_can_start_a_list_for_one_user(self):
-
-        # Edith has heard about a cool new online to-do app.
-        # She goes to check out its homepage
-        self.browser.get(self.live_server_url)
-
-        # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-        
-        # She is invited to enter a to-do item straight away
-        inputBox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputBox.get_attribute('placeholder'),
-            'Enter a to-do item'
-            )
-        
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        inputBox.send_keys('Buy peacock feathers')
-       
-    
-        # when she hits enter, the page updates, and now the page lists
-        # "1： Buy peacock feathers" as an item in a to-do list
-        inputBox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1:Buy peacock feathers')
-
-
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly"(Edith is very methodical)
-        inputBox = self.browser.find_element_by_id('id_new_item')
-        inputBox.send_keys('Use peacock feathers to make a fly')
-        inputBox.send_keys(Keys.ENTER)
-        
-        # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table('1:Buy peacock feathers')
-        self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
-        
-        #satisfied, she goes back to sleep
-        
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
@@ -132,6 +138,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertAlmostEqual(
             inputBox.location['x'] + inputBox.size['width']/2, 512, delta=10
         )
-
+    
+        
+    
 
        
